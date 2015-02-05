ee667860af94e3b50d7846007e162dec295a2ea7
Brian Bustin <brian@bustin.us>
2015-02-05 17:58:12 -0500

    Startup order was wrong causing data not to properly get logged to rrd.

--- start_web_controller.sh
+++ start_web_controller.sh
@@ -6,12 +6,12 @@ echo "========================================================================="
 echo "-------------------------------------------------------------------------"
 echo "Backend daemons"
 echo "-------------------------------------------------------------------------"
-echo "Starting component: rrdtool_logger"
-/usr/bin/screen -d -m -S rrdtool_logger python manage.py rrdtool_logger
 echo "Starting component: mqtt_bridge"
 /usr/bin/screen -d -m -S mqtt_bridge python manage.py mqtt_bridge
 echo "Starting component: programming_sentry"
 /usr/bin/screen -d -m -S programming_sentry python manage.py programming_sentry
+echo "Starting component: rrdtool_logger"
+/usr/bin/screen -d -m -S rrdtool_logger python manage.py rrdtool_logger
 
 echo "-------------------------------------------------------------------------"
 echo "UI"
