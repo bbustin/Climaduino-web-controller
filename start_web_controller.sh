4db86d5bb3a8f6d1acbb7a60af41f6f3f4484030
Brian Bustin <brian@bustin.us>
2015-02-12 09:24:36 -0500

    Quick hackey temporary fix for timing issues keeping rrdtool logging from working.

--- start_web_controller.sh
+++ start_web_controller.sh
@@ -6,12 +6,14 @@ echo "========================================================================="
 echo "-------------------------------------------------------------------------"
 echo "Backend daemons"
 echo "-------------------------------------------------------------------------"
+echo "Starting component: rrdtool_logger"
+/usr/bin/screen -d -m -S rrdtool_logger python manage.py rrdtool_logger
+# need to fix this in a better way later
+sleep 30 #there is a timing issue where logging fails if mqtt_bridge comes up before rrdtool_logger
 echo "Starting component: mqtt_bridge"
 /usr/bin/screen -d -m -S mqtt_bridge python manage.py mqtt_bridge
 echo "Starting component: programming_sentry"
 /usr/bin/screen -d -m -S programming_sentry python manage.py programming_sentry
-echo "Starting component: rrdtool_logger"
-/usr/bin/screen -d -m -S rrdtool_logger python manage.py rrdtool_logger
 
 echo "-------------------------------------------------------------------------"
 echo "UI"
